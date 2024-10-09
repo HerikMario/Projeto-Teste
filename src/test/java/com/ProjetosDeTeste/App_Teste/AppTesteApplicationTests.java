c1e557ebc58a353ddc28b5c1f256ebd2e62042d1
MatheusdCastro <matheusdcastrovieira@gmail.com>
2024-10-09 03:43:36 -0300

    Organização de pastas

---
+++ src/test/java/com/ProjetosDeTeste/App_Teste/AppTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjetosDeTeste.App_Teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
